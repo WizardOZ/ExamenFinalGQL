4366cbf4c2971bdebcd2c3e8a058bb9070c6c1c0
WizardOz <jbg_jeboga@hotmail.com>
2024-07-05 17:36:58 +0200

    v4

--- main.ts
+++ main.ts
@@ -8,15 +8,7 @@ import { Episode } from "./resolvers/episode.ts";
 import { typeDefs } from "./gql/schema.ts";
 import montoose from "mongoose";
 
-const API_KEY = Deno.env.get("API_KEY");
-if (!API_KEY) {
-  throw new Error("Please provide a MongoDB connection string");
-}
 
-// Connect to MongoDB
-await montoose.connect(API_KEY);
-
-console.info("🚀 Connected to MongoDB");
 
 const server = new ApolloServer({
   typeDefs,
